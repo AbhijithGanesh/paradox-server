f958fdf050b6f099d6909001d4a11d74e7bf7725
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-02-28 12:43:21 +0530

    Addition of ESlint; formatted Question.js; Updates to packages
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- app/db/Models/Question.js
+++ app/db/Models/Question.js
@@ -1,55 +1,50 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const QuestionSchema = new mongoose.Schema({
-    imageUrl: { 
-        type: String, 
-        required: true
-    },
-    answer: { 
-        type: String, 
-        required: true,
-        lowecase: true
-    },
-    levelNumber: {
-        type: Number,
-        required: true
-    },
-    hookEnabled: {
-        type: Boolean,
-        default: true
-    }
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+    lowecase: true,
+  },
+  levelNumber: {
+    type: Number,
+    required: true,
+  },
+  hookEnabled: {
+    type: Boolean,
+    default: true,
+  },
 });
 
-QuestionSchema.pre('save',async function(next){
-
-    try{
-        if(!this.hookEnabled){
-            this.hookEnabled = true
-            next()
-        }
-
-        const salt = await bcrypt.genSalt(10)
-        const hashedAnswer = await bcrypt.hash(this.answer, salt)
-        this.answer = hashedAnswer
-        next()
-    } catch(error){
-        next(error)
+QuestionSchema.pre("save", async function (next) {
+  try {
+    if (!this.hookEnabled) {
+      this.hookEnabled = true;
+      next();
     }
 
-})
+    const salt = await bcrypt.genSalt(10);
+    const hashedAnswer = await bcrypt.hash(this.answer, salt);
+    this.answer = hashedAnswer;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 QuestionSchema.methods.isValidAnswer = async function (answer) {
+  try {
+    return await bcrypt.compare(answer, this.answer);
+  } catch (error) {
+    throw error;
+  }
+};
 
-    try{
-        return await bcrypt.compare(answer, this.answer)
-    } catch(error){
-        throw(error)
-    }
-    
-}
+const Question = mongoose.model("question", QuestionSchema);
 
-const Question = mongoose.model('question', QuestionSchema)
-
-module.exports = Question
+module.exports = Question;
